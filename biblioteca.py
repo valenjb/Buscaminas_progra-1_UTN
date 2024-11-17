d50cdae99cd74acd4b09af1f97ee8f25cb1f5f83
ValentinoJimenoB <valenjimenob@gmail.com>
2024-11-17 18:30:13 -0300

    organizamos bases de funciones y config

--- biblioteca.py
+++ biblioteca.py
@@ -1,43 +1,9 @@
 import pygame
 import random
-
-pygame.font.init() 
-# CONSTS
-PANTALLA_ANCHO = 400
-PANTALLA_ALTO = 400
-RESOLUCION_PANTALLA = (PANTALLA_ANCHO, PANTALLA_ALTO)
-
-color_fondo = [127, 157, 235]
-posicion_personaje = [400, 300]
-
-# COLORES: RGB[A] (Red, Green, Blue) [Alpha] -> 0 - 255
-COLOR_ROJO = (255, 0, 0)
-COLOR_NARANJA = (245, 79, 7)
-COLOR_AZUL_CLARO = (127, 157, 235)
-COLOR_FUCSIA = (204, 6, 161)
-
-# Pantalla Principal
-# pantalla = pygame.display.set_mode((800, 600))
-# pantalla = pygame.display.set_mode(RESOLUCION_PANTALLA)
-pantalla = pygame.display.set_mode(RESOLUCION_PANTALLA) # Ventana maximizable
-# pantalla = pygame.display.set_mode((PANTALLA_ANCHO, PANTALLA_ALTO))
-pygame.display.set_caption("BUSCAMINA")
-
-# Imagenes:
-icono=pygame.image.load("BUSCAMINA-PROYECTO\pngegg.png")
-imagen_cuadrado=pygame.image.load("BUSCAMINA-PROYECTO\pngkit_white-button-png_2084464.png")
-pygame.display.set_icon(icono)
-imagen_cuadrado=pygame.transform.scale(imagen_cuadrado,(50,50))
-
-#FUENTES
-font_inicio=pygame.font.SysFont("Arial",36)
+from config import *
 
 
-#BOTON
-boton_jugar=pygame.Rect(PANTALLA_ANCHO/2-100,PANTALLA_ALTO/2-50,200,50)
-boton_salir=pygame.Rect(PANTALLA_ANCHO/2-100,PANTALLA_ALTO/2+50,200,50)
-texto_boton_jugar=font_inicio.render("Jugar",True,(0,0,0))
-texto_boton_salir=font_inicio.render("Salir",True,(255,255,255))
+
 #PANTALLA DE
 def pantalla_inicio():
     pantalla.fill((128, 0, 128))
@@ -67,7 +33,7 @@ def inicializar_matriz(cant_filas:int , cant_colum:int)->list:
     return matriz
 
 
-     
+
 
 def crear_matriz_buscaminas(cant_filas:int, cant_colum:int, minas:int)->list:
     matriz=inicializar_matriz(cant_filas,cant_colum)
@@ -80,11 +46,12 @@ def crear_matriz_buscaminas(cant_filas:int, cant_colum:int, minas:int)->list:
         if matriz[fila_random][columna_random]!=-1:
             matriz[fila_random][columna_random]=-1
             minas_colocadas+=1
-       
-    return matriz
-matriz=crear_matriz_buscaminas(8,8,20)
 
-for i in range(len(matriz)):
-    for j in range(len(matriz[i])):
-        print(matriz[i][j],end=" ")
-    print("")    
+    return matriz
+
+
+def test_matriz(matriz:list):
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            print(matriz[i][j],end=" ")
+        print("")    
--- main.py
+++ main.py
@@ -2,3 +2,38 @@ import pygame
 import random
 import json
 from biblioteca import *
+from config import *
+
+pygame.display.set_caption("BUSCAMINA")
+
+pygame.display.set_icon(icono)
+
+
+
+contador = 0
+corriendo = True
+mostrar_inicio=True
+while corriendo == True:
+
+    if mostrar_inicio:
+        pantalla_inicio()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                corriendo=False
+            if event.type ==pygame.MOUSEBUTTONDOWN:
+                if boton_jugar.collidepoint(event.pos):
+                    mostrar_inicio=False
+                if boton_salir.collidepoint(event.pos):
+                    corriendo=False
+    else:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                corriendo = False
+            if evento.type == pygame.MOUSEBUTTONDOWN:
+                print(evento)
+
+
+        #pantalla.blit(imagen_cuadrado,(50,0))
+        pygame.display.flip()
+
+pygame.quit()
