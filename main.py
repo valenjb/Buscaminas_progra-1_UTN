8cfb0e838c03e89275c3a779051b25a9f5d5b736
ValentinoJimenoB <valenjimenob@gmail.com>
2024-11-23 17:19:39 -0300

    agregamos banderas, falta arreglar cuadno se saca y la ft

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ pygame.display.set_icon(icono)
 contador = 0
 corriendo = True
 mostrar_inicio=True
+banderas = {} # Diccionario para las banderas
 
 while corriendo:
     if mostrar_inicio:
@@ -30,16 +31,35 @@ while corriendo:
     else:
         pantalla.fill(COLOR_TABLERO)
         crear_rectangulos(matriz_completa, estados, pantalla)
+
+        for (fila, columna) in banderas:             # Redibujar las banderas
+            x = desplazamiento_x + columna * 50
+            y = desplazamiento_y + fila * 50
+            pantalla.blit(imagen_bandera, (x, y))
+
         for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Clic izquierdo
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 mouse_x, mouse_y = event.pos
                 desplazamiento_x = (PANTALLA_ANCHO - len(tablero) * 50) // 2
                 desplazamiento_y = (PANTALLA_ALTO - len(tablero) * 50) // 2
                 fila = (mouse_y - desplazamiento_y) // 50
                 columna = (mouse_x - desplazamiento_x) // 50
                 if (fila, columna) in estados:
-                    estados[(fila, columna)] = False  # Descubrir casilla
-            
+                    estados[(fila, columna)] = False  # Cambia el estado  (bora la casilla y descubre el numero)
+
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+                mouse_x, mouse_y = event.pos
+                desplazamiento_x = (PANTALLA_ANCHO - len(tablero) * 50) // 2
+                desplazamiento_y = (PANTALLA_ALTO - len(tablero) * 50) // 2
+                fila = (mouse_y - desplazamiento_y) // 50
+                columna = (mouse_x - desplazamiento_x) // 50
+
+                if (fila, columna) in estados and estados[(fila, columna)] == True:  # Casilla cubierta
+                    if (fila, columna) not in banderas:
+                        banderas[(fila, columna)] = True  # Colocar bandera
+                    else:
+                        del banderas[(fila, columna)]  # Quitar bandera
+
             elif event.type == pygame.QUIT:
                 corriendo = False
                 print("cerrando juego...")
